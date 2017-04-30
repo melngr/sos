f49c0c5771bfdf82bdbb1b86d423a82e96c9de2f
Ben <BenjiHu@users.noreply.github.com>
2017-04-29 21:35:28 -0400

    documentation changes

--- src/items/ItemTextbook.cpp
+++ src/items/ItemTextbook.cpp
@@ -2,6 +2,7 @@
 #include "Item.h"
 #include "ItemTextbook.h"
 
+//make a new textbook
 ItemTextbook::ItemTextbook(std::string name, int maxuses) : Item(name, maxuses) {
 	_name = "Textbook on: " + name;
 }
@@ -9,6 +10,10 @@ ItemTextbook::ItemTextbook(std::string name, int maxuses) : Item(name, maxuses) 
 ItemTextbook::~ItemTextbook() {
 }
 
+/**
+Use a textbook which adds 10 * the number used as defense to the Player
+Applied via Player function
+*/
 int ItemTextbook::use(int consumed) {
 	if (_uses - consumed > 0) {
 		_uses -= consumed;
